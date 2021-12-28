e539c34fe18d33de108311fc1bc19e990ab5cbc0
นายพุฒิพงศ์  สักแสน <59078952+Senju14589@users.noreply.github.com>
2021-12-29 00:00:28 +0700

    layout app

--- Layout app/app/src/main/java/th/ac/kku/nkc/cis/layoutapplication/PhotoActivity2.kt
+++ Layout app/app/src/main/java/th/ac/kku/nkc/cis/layoutapplication/PhotoActivity2.kt
@@ -1,10 +1,8 @@
 package th.ac.kku.nkc.cis.layoutapplication
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 
 class PhotoActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
